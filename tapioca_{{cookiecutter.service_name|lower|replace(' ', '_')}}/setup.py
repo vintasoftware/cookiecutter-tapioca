b2207897adc1579f013a94b2314dfdb87f52afa4
Filipe Ximenes <filipeximenes@gmail.com>
2015-08-09 11:01:59 -0300

    adding oauthlib in case uses oauth

--- tapioca_{{cookiecutter.service_name|lower|replace(' ', '_')}}/setup.py
+++ tapioca_{{cookiecutter.service_name|lower|replace(' ', '_')}}/setup.py
@@ -20,7 +20,10 @@ except (IOError, ImportError):
 
 package = 'tapioca_{{ cookiecutter.service_name|lower|replace(' ', '_') }}'
 requirements = [
-    'tapioca-wrapper==0.5.1',
+    'tapioca-wrapper<0.6.0',
+    {% if cookiecutter.use_oauth_2 == 'y' or cookiecutter.use_oauth_1 == 'y' %}
+    'requests-oauthlib==0.4.2',
+    {% endif %}
 ]
 test_requirements = [
 
